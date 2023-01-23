19c8cf983f89c8d2bd5b374851d5abc886578fd2
Brad White <bradscottwhite@gmail.com>
2023-01-23 05:27:14 +0000

    second commit

--- index.js
+++ index.js
@@ -1,51 +1,6 @@
 const chromium = require('chrome-aws-lambda');
 //https://console.aws.amazon.com/
 
-/*exports.handler = async (event, context, callback) => {
-  //const params = JSON.parse(event.body);
-
-  let res = null;
-  let browser = null;
-
-  try {
-    browser = await chromium.puppeteer.launch({
-      args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath,
-      headless: chromium.headless,
-      ignoreHTTPSErrors: true
-    });
-
-    let page = await browser.newPage();
-
-		await page.goto(event.url); // || params.url);
-
-    let title = await page.title();
-    let body = { title };
-
-    res = {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-        "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
-        "Access-Control-Allow-Methods" : "OPTIONS,POST",
-        "X-Requested-With" : "*"
-      },
-      body: JSON.stringify(body)
-    };
-
-  } catch (error) {
-    return callback(error);
-  } finally {
-    if (browser !== null)
-      await browser.close();
-  }
-
-  return callback(null, res);
-};*/
-
 exports.handler = async event => {
 	let url = '';
 
